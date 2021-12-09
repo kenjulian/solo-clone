42b3a77d5a1701eced0aca2df0b5aa3d5f3d91f7
kenjulian <kenneth.julian@yahoo.com>
2021-12-09 01:58:19 -0500

    added update route

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
+
 const { validatePostInput } = require('../../utils/validation');
+//authenticates user to access and use the song
 const {requireAuth} = require('../../utils/auth');
 const {Song} = require('../../db/models/song');
 
@@ -18,21 +20,39 @@ router.get("/", asyncHandler( async(req, res) => {
 //get one song
 router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
         const oneSong = await Song.findByPk(req.params.id);
+        
         return res.json(oneSong);
     })
   );
 
 //post a song
 router.post("/", requireAuth, validatePostInput, asyncHandler(async (req, res) => {
-    const song = await Song.create(req.body);
+    const {songUrl, picUrl, title} = req.body;
+    const song = await Song.create({songUrl, picUrl, title});
     res.json(song)
-}))
+})) //why req.body doesn't work for .create
+
+//update a song
+router.patch("/:id(\\d+)", requireAuth, validatePostInput, asyncHandler(async (req, res) => {
+    const update = await Song.update(
+      { songUrl, picUrl, title},
+      { where: {id: req.params.id},
+    }
+    );
+    res.json(update);
+  })
+);
+
 
 //delete a song
 router.delete("/:id(\\d+)",requireAuth, asyncHandler(async function(req, res) {
     const deleteSong = await Song.findByPk(req.params.id);
-    await deleteSong.destroy();
-    return res.json(req.body);
+
+    if (deleteSong) {
+       deleteSong.destroy();
+    } else {
+    res.status("404").json({Message: "Not found"})
+  }
 })
 );
 module.exports = router;
